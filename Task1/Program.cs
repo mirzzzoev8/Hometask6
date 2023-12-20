ddfa2f43f11c3973d5a22961950951192e179860
mirzzzoev8 <mirzoevyusufjon8@gmail.com>
2023-12-20 20:39:32 +0500

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿class Teacher {
+     string fullName;
+     string subject;
+     int experience;
+
+
+     public Teacher(){}
+    public Teacher(string name, string sub){
+        fullName = name;
+        subject = sub ;
+    }
+    public Teacher(string name, string sub, int experience){
+        fullName = name;
+        subject = sub ;
+        this.experience = experience;
+    }
+
+    
+    public string Teach(){
+        return $"{fullName} is teaching {subject}";
+    }
+    public void SetExperience(int years){
+        experience = years;
+    }
+    public int GetExperience(){
+        return experience;
+    }
+    
+    
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿class Student{
+    string fullname;
+    int age;
+    int grade;
+    public Student(){}
+    public Student(string name, int age){
+        fullname = name;
+        this.age = age;
+    }
+    public Student(string name,  int age, int grade){
+          fullname = name;
+          this.age = age;
+          this.grade = grade;
+    }
+    public string Study(){
+         return $"{fullname} is teaching ";
+    }
+    public void SetGrade(int newGrade){
+        grade = newGrade;
+    }
+    public int GetGrade(){
+       return grade;
+    }
+}
